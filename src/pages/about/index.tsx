49852778b19ffae48a65e99a4613316e629b262a
goeun <33068456+goeunpark@users.noreply.github.com>
2025-01-22 18:50:07 -0500

    Update index.tsx

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -5,16 +5,11 @@ const About: NextPage = () => {
   return (
     <Layout>
       <div className="page-content about-page">
-        <p>Goeun Park is a writer and computer science educator.</p>
+        <p>Goeun Park (b. Busan 1994) is a writer from Northern Minnesota.</p>
         <p>
-          Goeun has a BA in Psychology from Pomona College and DACA from the
-          United States government. Formerly, they worked as a research
-          specialist, software engineer, and hotline crisis counselor.
-          Currently, they are working on a speculative short story collection
-          and this website.
+          They are currently a Truman Capote Fellow at the Iowa Writers' Workshop.
         </p>
         <p>
-          <a href="https://read.cv/goeun">cv</a>
           <br></br>
           <a href="mailto:contact@goeun.co">contact@goeun.co</a>
         </p>
