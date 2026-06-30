aa582aba87beffc78f63f650f26f5a2a70bcada1
goeunpark <goeuntothepark@gmail.com>
2026-06-30 15:08:08 -0500

    Fix css on landing page

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -12,7 +12,7 @@ type Props = {
 const PostPreview = ({ title, date, excerpt, author, slug }: Props) => {
   return (
     <div className={styles.preview}>
-      <Link as={`/blog/${slug}`} href="/blog/[slug]">
+      <Link as={`/blog/${slug}`} href="/blog/[slug]" passHref>
         <span className={styles.title}>{title}</span>
         <p>
           <i>{date}</i>
